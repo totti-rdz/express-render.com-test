3647aaa6c53639a39bfeeef469d69858e7134769
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-03 20:52:28 +0200

    create /again get endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/again".at, (req, res) => {
+  res.send("Hello again!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
